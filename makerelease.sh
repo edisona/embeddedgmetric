058b680b1970c11e0753b6368bba0609ab5f541f
nickgsuperstar <nickgsuperstar@f42bce2d-442a-0410-bc3c-4fba4d9da64b>
2007-05-17 19:07:32 +0000

    Be safe and zap tarball

--- makerelease.sh
+++ makerelease.sh
@@ -7,7 +7,7 @@ svn co http://embeddedgmetric.googlecode.com/svn/trunk ${FILE}
 find $FILE -name '.svn' | xargs rm -rf
 
 rm -f ${FILE}/makerelease.sh
-
+rm -f ${FILE}.tar.gz
 tar -czvf ${FILE}.tar.gz ${FILE}
 rm -rf ${FILE}
 echo "DONE"
