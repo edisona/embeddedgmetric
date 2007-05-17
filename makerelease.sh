60a5855e644e3d72c193b312d5bec275d14aef28
nickgsuperstar <nickgsuperstar@f42bce2d-442a-0410-bc3c-4fba4d9da64b>
2007-05-17 19:04:25 +0000

    Bump to v1.1.0 Add release maker

---
+++ makerelease.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+FILE=embeddedgemtric-v1.1.0
+rm -rf ${FILE}
+svn co http://embeddedgmetric.googlecode.com/svn/trunk ${FILE}
+
+find $FILE -name '.svn' | xargs rm -rf
+
+rm -f ${FILE}/makerelease.sh
+
+tar -czvf ${FILE}.tar.gz ${FILE}
+rm -rf ${FILE}
+echo "DONE"
+ls -lh ${FILE}.tar.gz
